871a410b6e40c9b7994e2386311890c66d7f3e08
Sarah Tiefert <sarah.tiefert@htwg-konstanz.de>
2021-04-01 10:17:41 +0200

    defined station

--- first_exercise/customer.py
+++ first_exercise/customer.py
@@ -1,13 +1,23 @@
 class Customer:
-    def __init__(self, name, stationVisits):
+    def __init__(self, name, stationVisits, timeNextCustomer, shoppingList):
         self.name = name
         self.stationVisits = stationVisits
+        self.timeNextCustomer = timeNextCustomer
+        self.shoppingList = shoppingList
+        # todo: Einkaufliste
+
+    def productsForStation(self, station):
+        return self.shoppingList[station]
 
     def startShopping(self):
+        # create event to enter station
+        self.stationVisits.next
         return
 
     def arriveStation(self):
+        # crea
         return
-        
+
     def leaveStation(self):
+
         return
--- first_exercise/station.py
+++ first_exercise/station.py
@@ -1,10 +1,21 @@
+import event
+import event_queue
+
 class Station:
     def __init__(self, name, servingTime):
         self.name = name
         self.servingTime = servingTime
+        self.customerQueue = []  # todo: queue
 
     def queueCustomer(self, customer):
+        self.customerQueue.append(customer)
         return
         
-    def serveCustomer(self, customer):
+    def serveCustomer(self, func):
+        # calculate time customer needs (servingTime * customerObjects)
+        customer = self.customerQueue.pop()
+        timeNeeded = self.servingTime * customer.productsForStation(self.name)
+
+        # create "customer serve" event
+        func(timeNeeded)
         return
