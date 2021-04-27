869c30f3c6414aa18e5a571140d73a2fe15f6c68
Alexander Brese <admin@alexanderbrese.com>
2021-04-27 15:14:38 +0200

    feat: basic statistics layout

---
+++ second_exercise/statistics.py
@@ -0,0 +1,33 @@
+class Statistics:
+    def __init__(self, customers):
+        self._servedCustomers = []
+        self.droppedStations = {}
+        self._completeShoppingTimes = {}
+        self.customers = customers
+
+    def setLastCustomerDate(self, customer, date):
+        return
+
+    def addServedCustomer(self, customer):
+        return
+
+    def setCompleteShoppingTime(self, customer, time):
+        return
+
+    def addDroppedStationPercentage(self, droppingCustomers):
+        return
+
+    def lastCustomerDate(self):
+        return
+    
+    def servedCustomers(self):
+        return
+
+    def completeShoppingTimes(self):
+        return
+
+    def averageCompleteShppingTime(self):
+        return
+    
+    def droppedStationPercentages(self):
+        return
